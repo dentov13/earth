ce7ba6775a5873998aff9ad1ad3da80945029286
dentov13 <tovstokhatko.denis@gmail.com>
2018-11-26 18:52:55 +0200

    starfield background globe logic

--- src/views/Starfield/Starfield.jsx
+++ src/views/Starfield/Starfield.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
+import * as THREE from 'three';
+
 import bg from 'assets/images/starfield.png';
 
 const Background = styled.div`
-    background: url(${bg}) no-repeat center center;
     width: 100%;
     height: 100%;
     position: absolute;
@@ -11,8 +12,40 @@ const Background = styled.div`
     left: 0;
 `;
 
-export default function Starfield() {
-    return (
-        <Background />
-    );
+export default class Starfield extends Component {
+    componentDidMount() {
+        let renderer = new THREE.WebGLRenderer();
+        let scene = new THREE.Scene();
+        let aspect = window.innerWidth / window.innerHeight;
+        let camera = new THREE.PerspectiveCamera(45, aspect, 0.1, 1500);
+
+        let textureLoader = new THREE.TextureLoader();
+        let galaxyGeometry = new THREE.SphereGeometry(100, 30, 30);
+        let galaxyMaterial = new THREE.MeshBasicMaterial({
+            side: THREE.BackSide
+        });
+        let galaxy = new THREE.Mesh(galaxyGeometry, galaxyMaterial);
+        textureLoader.crossOrigin = true;
+        textureLoader.load(bg, function (texture) {
+            galaxyMaterial.map = texture;
+            scene.add(galaxy);
+        })
+
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        // may to append DOM element
+        scene.add(camera);
+
+        let render = function () {
+            requestAnimationFrame(render);
+            renderer.render(scene, camera);
+        };
+
+        render();
+    }
+
+    render() {
+        return (
+            <Background />
+        );
+    }
 }
